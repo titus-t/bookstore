98b14141e154e00f34a4ceb96d5e804b28184582
titus-t <titus.torvelainen@myy.haaga-helia.fi>
2026-02-02 22:33:37 +0200

    Template luotu

--- src/main/java/fi/haagahelia/bookstore/web/BookController.java
+++ src/main/java/fi/haagahelia/bookstore/web/BookController.java
@@ -2,14 +2,12 @@ package fi.haagahelia.bookstore.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BookController {
 
     @GetMapping("/index")
-    @ResponseBody
     public String showIndex() {
-        return "Welcome to the Bookstore!";
+        return "index";
     }
 }
