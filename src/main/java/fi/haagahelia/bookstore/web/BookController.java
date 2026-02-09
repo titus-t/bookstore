4066ace214d6236db1135a811f42c1eb69ff1c94
titus-t <titus.torvelainen@myy.haaga-helia.fi>
2026-02-09 14:05:41 +0200

    Edit tehty

--- src/main/java/fi/haagahelia/bookstore/web/BookController.java
+++ src/main/java/fi/haagahelia/bookstore/web/BookController.java
@@ -43,4 +43,11 @@ public class BookController {
     	repository.deleteById(bookId);
         return "redirect:../booklist";
     }   
+
+
+@RequestMapping(value="/edit/{id}")
+public String editBook(@PathVariable("id")Long bookId, Model model) {
+	model.addAttribute("book",repository.findById(bookId).get());
+	return "editbook";
+}
 }
